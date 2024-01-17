fd361a13e65ef07cff8eaef0f5be45de17830b9f
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-01-16 21:18:14 -0500

    implemented social media links

--- frontend_react/src/component/SocialMedia.jsx
+++ frontend_react/src/component/SocialMedia.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import {BsTwitter, BsInstagram} from 'react-icons/bs';
-import {FaFacebookF} from 'react-icons/fa';
+import {BsInstagram} from 'react-icons/bs';
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 /* Higher order components */
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        <div>
-            <BsTwitter />
+      <div>
+        <a href='https://www.linkedin.com/in/jeffrey-huang-yz' target="_blank" rel="noreferrer noopener">
+          <FaLinkedin />
+        </a> 
+      </div>
 
-        </div>
-        <div>
-            <FaFacebookF />
-            
-        </div>
-        <div>
-            <BsInstagram />
-            
-        </div>
+      <div>
+        <a href='https://github.com/jeffrey-huang-yz' target="_blank" rel="noreferrer noopener">
+          <FaGithub />
+        </a>  
+      </div>
+
+      <div >
+        <a href='https://www.instagram.com/jeffrxyh/' target="_blank" rel="noreferrer noopener">  
+          <BsInstagram />
+        </a>
+      </div>
+      
     </div>
   )
 }
